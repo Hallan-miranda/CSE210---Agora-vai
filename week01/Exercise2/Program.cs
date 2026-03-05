4957711d5d5b8ee743a0bb3c10aae7384854c038
Hallan Miranda <hallanlmm@gmail.com>
2026-03-05 08:21:13 -0300

    exercise 2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,47 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What is you grade percentage? ");
+        string userInput = Console.ReadLine();
+        int percentage = int.Parse(userInput);
+        string letterGrade;
+        string grade;
+
+        if (percentage >= 90)
+        {
+            letterGrade = "A";
+        }
+        else if (percentage >= 80)
+        {
+            letterGrade = "B";
+        }
+         else if (percentage >= 70)
+        {
+            letterGrade = "C";
+        } else if (percentage >= 60)
+        {
+            letterGrade = "D";
+        }
+        else
+        {
+            letterGrade = "F";
+        }
+        
+        int lastDigit = percentage % 10;
+
+        if (lastDigit >=7)
+        {
+            grade = letterGrade + "+";
+        }
+        else if (lastDigit <3)
+        {
+            grade = letterGrade +"-";
+        }
+        else
+        {
+            grade = letterGrade;
+        }
+
+        Console.WriteLine($"Your Letter Grade is...   {grade}");
     }
 }
