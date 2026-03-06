2a93da58230396be44632f611565b02046509b4b
Hallan Miranda <hallanlmm@gmail.com>
2026-03-06 11:52:28 -0300

    resume exercise

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        
+        Resume resume = new Resume();
+
+        resume._name = "Hallan Miranda";
+
+        Job job1 = new Job();
+        job1._company = "ABIJSUD";
+        job1._jobTitle = "Admsintrative Assistent";
+        job1._startYear = 2025;
+        job1._endYear = 2030;
+
+        Job job2 = new Job();
+        job2._company = "Metanoe";
+        job2._jobTitle = "CTO";
+        job2._startYear = 2022;
+        job2._endYear = 2025;
+
+
+        resume._jobs.Add(job1);
+        resume._jobs.Add(job2);
+        resume.Display();
     }
 }
